b1701ca676fcab012280e9c7879166401b466ea0
hutingung <tingung.hu@aurea.com>
2021-04-23 22:41:07 +0800

    Update blog categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BlogsComponent} from './blogs/blogs.component';
+import {RouterModule, Routes} from '@angular/router';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {HeadersComponent} from './shared/headers/headers.component';
+import {FootersComponent} from './shared/footers/footers.component';
+import {DemoMaterialModule} from './material-module';
+
+
+const appRoutes: Routes = [
+  {path: 'blogs', component: BlogsComponent},
+  {path: '', redirectTo: '/blogs', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogsComponent,
+    PageNotFoundComponent,
+    HeadersComponent,
+    FootersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    ),
+    DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
